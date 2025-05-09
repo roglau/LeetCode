2f0446d909d13d691ed494e714c0e6731e17e014
Roger <126433612+roglau@users.noreply.github.com>
2025-05-09 23:36:58 +0700

    Time: 19 ms (75.55%), Space: 79.6 MB (87.39%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,26 +1,26 @@
-class Solution {
-public:
+class Solution{
+    public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> s;
         int area = 0;
-        s.push(-1);
+        // s.push(-1);
         for(int i = 0;i<heights.size();i++){
             // cout << "topidx : " << s.top() << " area : " << area << endl;
-            while(!s.empty() && s.top() != -1 && heights[i] <= heights[s.top()]){
+            while(!s.empty() && heights[i] <= heights[s.top()]){
                 int h = heights[s.top()];
                 s.pop();
-                int w = i - s.top() - 1;
+                int w = s.empty() ? i : i - s.top() - 1;
                 area = max(area, h*w);
             }
 
             s.push(i);
         }
 
-        while(!s.empty() && s.top() != -1){
+        while(!s.empty()){
             // cout << "bwh : " << s.top() << endl;
             int h = heights[s.top()];
             s.pop();
-            int w = heights.size() - s.top() - 1;
+            int w = s.empty() ? heights.size() : heights.size() - s.top() - 1;
             area = max(area, h*w);
         }
 
