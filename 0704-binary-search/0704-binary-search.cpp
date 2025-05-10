11439db84ab87403b1dbe82d1f5683b56346a1b7
Roger <126433612+roglau@users.noreply.github.com>
2025-05-10 23:40:07 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,21 +3,19 @@ class Solution{
     int search(vector<int>& nums, int target) {
         int l = 0, r = nums.size();
 
-        while(l < r){
+        while(l <= r){
             int mid = ((l+r)/2);
             // cout << l << " <- -> " << r << " = " << mid << endl;
-
-            if(target > nums[mid] && mid != l){
-                l = mid;
-            }else if(target < nums[mid] && mid != r){
-                r = mid;
-            }else if(target == nums[mid]){
+            if(target == nums[mid]){
                 return mid;
-            }else{
-                return -1;
+            }else if(target < nums[mid]){
+                r = mid - 1;
+            }else {
+                l = mid + 1;
             }
         }
 
         return -1;
     }
+
 };
